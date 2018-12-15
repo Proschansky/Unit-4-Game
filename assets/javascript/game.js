c530433bf738880fc64ec040f82051cf53e50d4f
Brzeczyszczykiewicz <Samproschansky@gmail.com>
2018-12-14 19:48:10 -0500

    Fixed the game with a button. Now to style it a bit better!

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,10 +2,11 @@ var gameStart = {
     targetNumber: Math.floor(Math.random() * 119) + 1,
     wins: 0,
     losses: 0,
-    totalPoints: 0 //This may not be needed and need to recheck jquery further down
 };
 
 var targetNumber = gameStart.targetNumber;
+
+var newTargetNumber = gameStart.targetNumber;
 
 $("#number-to-guess").text(targetNumber);
 
@@ -13,9 +14,6 @@ var counter = 0;
 
 
 // Ideally this should create a range function and into which I can feed the length of the array and it can generate a random image out of the images array.
-function range(size) {
-  return [...Array(size).keys()].map(i => i);
-}
 
 var numbers = [1,2,3,4,5,6,7,8,9,10,11,12,]
 
@@ -26,23 +24,6 @@ function shuffle(o) {
 
 console.log(shuffle(numbers))
 
-// var numberOptions = {
-    // optionOne: Math.floor(Math.random() * 11) + 1,
-    // optionTwo: Math.floor(Math.random() * 11) + 1,
-    // optionThree: Math.floor(Math.random() * 11) + 1,
-    // optionFour: Math.floor(Math.random() * 11) + 1,
-// }
-
-//var options =[, numberOptions.optionTwo, numberOptions.optionThree, numberOptions.optionFour];
-
-// console.log(range(options.length));
-
-// while (options[i] = options[i+1]||options[i+2]||options[i+3]||options[i-3]||options[i-2]||options[i-1]){
-  // options[i]= Math.floor(Math.random() * 11) + 1
-// }
-
-// console.log(options);
-
 var images = ["assets/images/crystalOne.jpg","assets/images/crystalTwo.jpg","assets/images/crystalThree.jpg","assets/images/crystalFour.jpg"];
 var randomImage = Math.floor(Math.random()*images.length);
 
@@ -50,29 +31,30 @@ for (i=0;i<images.length;i++){
   numbers[i] = shuffle(numbers[i])
 }
 
-empty = [];
+
 
 
 // Creates a random array of numbers out of four in order to generate a random image.
-for (i=0;i<images.length;i++){
-  empty.push(i);
-  shuffle(empty);
-}
+// for (i=0;i<images.length;i++){
+  // empty.push(shuffle(images[i]);
+// }
 
-console.log(empty)
-
-// Creates a for loop for every number option.
-for (var i = 0; i < empty.length; i++) {
+// Creates random images out of the list.
+$(".button").on("click", function(){
+  $(imageCrystal).removeClass("crystal-image")
+  
+  for (var i = 0; i < images.length; i++){
 
   // For each iteration, we will create an imageCrystal
   var imageCrystal = $("<img>");
 
   // First each crystal will be given the class ".crystal-image".
   // This will allow the CSS to take effect.
-  imageCrystal.addClass("crystal-image");
+  imageCrystal.addClass("crystal-image")
 
   // Each imageCrystal will be given a random src link from the images Array.
-  imageCrystal.attr("src", images[empty[i]]);
+  
+  imageCrystal.attr("src", shuffle(images[i]));
 
   // Each imageCrystal will be given a data attribute called data-crystalValue.
   // This data attribute will be set equal to the array value.
@@ -80,12 +62,12 @@ for (var i = 0; i < empty.length; i++) {
 
   // Lastly, each crystal image (with all it classes and attributes) will get added to the page.
   $("#crystals").append(imageCrystal);
-}
+  }
 
 // This time, our click event applies to every single crystal on the page. Not just one.
 $(".crystal-image").on("click", function() {
 
-  var crystalValue = ($(this).attr("data-crystalvalue"));
+  var crystalValue = $(this).attr("data-crystalvalue")
   crystalValue = parseInt(crystalValue);
   // We then add the crystalValue to the user's "counter" which is a global variable.
   // Every click, from every crystal adds to the global counter.
@@ -96,14 +78,18 @@ $(".crystal-image").on("click", function() {
 
   if (counter === targetNumber){
     alert("You win!");
+    counter = 0;
     gameStart.wins++;
-    $("#crystals").after("<h2>Wins: "+ gameStart.wins + "</h2>")
+    $("#crystals").html("<h2>Wins: "+ gameStart.wins + "</h2>");
+    $("#crystals").remove(imageCrystal);
   }
 
   else if (counter >= targetNumber) {
     alert("You lose!!");
+    counter = 0;
     gameStart.losses++;
-    $("#crystals").after("<h2>Losses: "+ gameStart.losses + "</h2>")
-  }}
-
-);
+    $("#crystals").html("<h2>Losses: "+ gameStart.losses + "</h2>");
+    $("#crystals").remove(imageCrystal);
+    }
+  });
+});
